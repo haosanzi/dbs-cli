2f1984b39fb659138fa0eaf0fc9d2914bc6b9d04
fengshifang <fengshifang@linux.alibaba.com>
2023-05-11 03:49:14 -0400

    test_resource: Add test resources for tdshim
    Signed-off-by: fengshifang <fengshifang@linux.alibaba.com>

--- dbs_test/test_tdx.sh
+++ dbs_test/test_tdx.sh
@@ -6,8 +6,8 @@ rm -rf /tmp/vsock.sock
 /root/data/xutong/dbs-cli/target/debug/dbs-cli \
   --log-file dbs-cli.log --log-level DEBUG \
   --serial-path /tmp/dbs   \
-  --kernel-path /root/data/xutong/image/bzImage \
-  --firmware-path /root/data/xutong/image/final-boot-kernel.bin  \
-  --rootfs /root/data/xutong/image/rootfs.img.alpine \
-  --boot-args "root=/dev/sda1 tdx_disable_filter  systemd.unit=kata-containers.target agent.log=debug agent.log_vport=1025" \
+  --kernel-path /root/data/xutong/dbs-cli/dbs_test/bzImage   \
+  --firmware-path /root/data/xutong/dbs-cli/dbs_test/final-boot-kernel.bin  \
+--rootfs /root/data/xutong/dbs-cli/dbs_test/rootfs.alpine  \
+--boot-args "root=/dev/sda1 tdx_disable_filter  systemd.unit=kata-containers.target agent.log=debug agent.log_vport=1025" \
   --vsock /tmp/vsock.sock create ;
